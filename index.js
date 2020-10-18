c39cad6b58f8f5e0ad40d24562add7249bf27676
Asuka109 <1097198892@qq.com>
2020-10-18 18:49:47 +0800

    feat: 修改转发机制为储存帧后周期性合并转发帧

--- index.js
+++ index.js
@@ -2,24 +2,64 @@ const WebSocketServer = require('./websocket')
 
 const wss = new WebSocketServer({ port: 3000 })
 
-wss.on('connection', ws => {
-  console.log(`[SERVER] Connection`)
+const send = (ws, data) => {
+  const _data = typeof data === 'string' ? data : JSON.stringify(data)
+  ws.send(_data)
+}
+
+let playerCount = 0
+let actionPool = []
+
+wss.on('connection', (ws, req) => {
+  console.log(`[SERVER] Connection: [${req.connection.remoteAddress}]`)
 
   ws.on('message', msg => {
     console.log(`[SERVER] Received: ${msg}`)
-    setTimeout(() => {
-      wss.broadcast(msg, err => err && console.log(`[SERVER] Error: ${err}`))
-    }, Math.random() * 100)
-    
+    const frame = JSON.parse(msg)
+    if (frame.frameType === 'InfoFrame') {
+      if (frame.info === 'ready') {
+        playerCount ++
+        if (playerCount >= 2)
+          wss.broadcast(msg, err => err && console.log(`[SERVER] Error: ${err}`))
+      }
+      return
+    }
+    if (frame.frameType === 'ActionFrame') {
+      actionPool.push(frame.actions)
+    }
   })
   
-  ws.send(JSON.stringify({
+  send(ws, {
     id: 0,
     frameType: 'StatusFrame',
     status: [
       { userId: "asd", pos: [1000, 1000] }
     ]
-  }))
+  })
 })
+
+setInterval(() => {
+  const actionsDict = {}
+  actionPool.forEach(actions => {
+    actions.forEach(action => {
+      if (action.userId in actionsDict) {
+        const [x, y] = actionsDict[action.userId].movement
+        const [_x, _y] = action.movement
+        actionsDict[action.userId].movement = [x + _x, y + _y]
+      } else {
+        actionsDict[action.userId] = action
+      }
+    })
+  })
+  actionPool = []
+  const actions = Object.values(actionsDict)
+  const frame = {
+    id: 0,
+    frameType: 'ActionFrame',
+    actions
+  }
+  wss.broadcast(frame)
+
+}, 1000 / 30)
 
 console.log('[SERVER] Listen on port 3000')
--- websocket.js
+++ websocket.js
@@ -1,13 +1,15 @@
 const WebSocket = require('ws')
 
 const returnAsIs = clients => clients
+const defaultSendCallback = err => err && console.log(`[SERVER] Error: ${err}`)
 
 class WebSocketServer extends WebSocket.Server {
-  broadcast(data, cb, filter=returnAsIs) {
+  broadcast(data, filter=returnAsIs, cb=defaultSendCallback) {
+    const _data = typeof data === 'string' ? data : JSON.stringify(data)
     filter(this.clients).forEach(
       client => {
         if (client.readyState === WebSocket.OPEN)
-          client.send(data, cb);
+          client.send(_data, cb);
       }
     )
   }
